1306105ac9df558dd85a878d237f5dd1a06cfc80
Davi Borges <davifelipe_300@hotmail.com>
2023-09-14 10:56:40 -0300

    adiciona rotas do angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarNotasComponent } from './components/notas/listar-notas/listar-notas.component';
+import { CriarNotaComponent } from './components/notas/criar-nota/criar-nota.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'notas/listar',
+    pathMatch: 'full' 
+  },
+  {
+    path: 'notas/listar',
+    component: ListarNotasComponent
+  },
+  {
+    path: 'notas/criar',
+    component: CriarNotaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
