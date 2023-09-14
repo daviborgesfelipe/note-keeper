0bff1263f42590cb0e845c1287f9c501c4df5ead
Davi Borges <davifelipe_300@hotmail.com>
2023-09-14 10:59:05 -0300

    altera de interface para class Nota, e add Type

--- src/app/components/notas/notas.ts
+++ src/app/components/notas/notas.ts
@@ -1,6 +1,21 @@
-export interface Nota {
+export class Nota {
   id?: number;
   titulo: string;
   conteudo: string;
-  tema: string;
+  tema: Tema;
+
+
+  constructor( 
+    titulo: string,
+    conteudo: string,
+    tema: Tema,
+    id?: number
+  ) {
+    this.titulo = titulo;
+    this.conteudo = conteudo;
+    this.tema = tema; 
+    this.id = id;    
+  }
 }
+
+type Tema = 'primary' | 'info' | 'warning' | 'danger' | 'dark'
