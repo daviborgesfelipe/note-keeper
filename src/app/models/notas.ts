5e8b7b7510a33779658e80350a2409bc57a58d97
Davi Borges <davifelipe_300@hotmail.com>
2023-09-16 00:10:34 -0300

    adiciona pastas para model e servico de notas

---
+++ src/app/models/notas.ts
@@ -0,0 +1,28 @@
+import { Categoria } from "./categoria";
+
+export class Nota {
+  id?: number;
+  titulo: string;
+  conteudo: string;
+  tema: Tema;
+  categoriaId: number;
+  categoria?: Categoria
+
+  constructor( 
+    titulo: string,
+    conteudo: string,
+    tema: Tema,
+    categoriaId: number,
+    id?: number,
+    categoria?: Categoria
+  ) {
+    this.titulo = titulo;
+    this.conteudo = conteudo;
+    this.tema = tema; 
+    this.categoriaId = categoriaId 
+    this.id = id;   
+    this.categoria = categoria
+  }
+}
+
+type Tema = 'primary' | 'info' | 'warning' | 'danger' | 'dark'
