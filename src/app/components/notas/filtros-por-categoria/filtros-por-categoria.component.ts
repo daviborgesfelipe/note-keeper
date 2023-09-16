75c9327b6c3c87561bb174dbcfbaf9f9f750ca6b
Davi Borges <davifelipe_300@hotmail.com>
2023-09-16 00:06:36 -0300

    inicia o componente de fitro por categoria

---
+++ src/app/components/notas/filtros-por-categoria/filtros-por-categoria.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Categoria } from 'src/app/models/categoria';
+
+@Component({
+  selector: 'app-filtros-por-categoria',
+  templateUrl: './filtros-por-categoria.component.html',
+  styleUrls: ['./filtros-por-categoria.component.css']
+})
+export class FiltrosPorCategoriaComponent {
+  @Input({required: true}) categorias: Categoria[] = [];
+
+  @Output() onFiltroSelecionado: EventEmitter<Categoria | null>;
+
+  constructor(){
+    this.onFiltroSelecionado = new EventEmitter();
+  }
+
+  selecionarTodas(){
+    this.onFiltroSelecionado.emit(null);
+  }
+
+  selecionarComFiltro(categoria: Categoria){
+    this.onFiltroSelecionado.emit(categoria)
+  }
+}
