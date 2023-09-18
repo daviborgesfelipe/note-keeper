cfdef7fe2675a1d1a901cdb94e985ea2e6ab76a0
Davi Borges <davifelipe_300@hotmail.com>
2023-09-18 15:10:15 -0300

    modifica mensagem do toast

--- src/app/components/categorias/criar-categoria/criar-categoria.component.ts
+++ src/app/components/categorias/criar-categoria/criar-categoria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Categoria } from 'src/app/models/categoria';
@@ -10,6 +10,7 @@ import { NotaService } from 'src/app/services/nota.service';
   templateUrl: './criar-categoria.component.html',
   styleUrls: ['./criar-categoria.component.css']
 })
+
 export class CriarCategoriaComponent {
   categoria : Categoria;
 
@@ -17,19 +18,20 @@ export class CriarCategoriaComponent {
     private categoriaService: CategoriaService,
     private router: Router,
     private toastService: ToastrService
-    ) {
-    this.categoria = new Categoria(
-      ''
-    );
+  ) {
+    this.categoria = new Categoria('');
   }
+  // @Output() categoriaCriada = new EventEmitter<Categoria>();
+
   criarCategoria(){
     this.categoriaService.criar(this.categoria).subscribe((_categoria) => {
       this.toastService.success(
-        `Nota ${this.categoria.titulo} CRIADA com sucesso`,
+        `Categoria ${this.categoria.titulo} CRIADA com sucesso`,
         'Sucesso'
-      )
+      );
 
+      // this.categoriaCriada.emit(_categoria);
       this.router.navigate(['/categorias', 'listar'])
-    })
+    });
   }
 }
