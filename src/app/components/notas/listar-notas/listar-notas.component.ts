d2381daeca2fc91434f13e4681dd6e896913ea02
Davi Borges <davifelipe_300@hotmail.com>
2023-09-18 15:18:03 -0300

    implementa filtros e adiciona eventemitter, output

--- src/app/components/notas/listar-notas/listar-notas.component.ts
+++ src/app/components/notas/listar-notas/listar-notas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Nota } from '../../../models/notas';
 import { NotaService } from '../../../services/nota.service';
 import { Categoria } from 'src/app/models/categoria';
+import { CategoriaService } from 'src/app/services/categoria.service';
 
 @Component({
   selector: 'app-listar-notas',
@@ -10,13 +11,50 @@ import { Categoria } from 'src/app/models/categoria';
 })
 export class ListarNotasComponent implements OnInit{
   notas: Nota[] = [];
-  categorias: Categoria[] = []
+  categorias: Categoria[] = [];
 
-  constructor(private notaService: NotaService) {
-    console.log("======>", this.notas)
+  filtroCategoria: Categoria;
+  nota: Nota;
+
+  constructor(
+      private notaService: NotaService, 
+      private categoriaService: CategoriaService
+    ) {
+    this.filtroCategoria = new Categoria('')
+    this.nota = new Nota('','','dark',0)
   }
-  
+    
   ngOnInit(): void {
-    this.notaService.selecionarTodos().subscribe(_notas => this.notas = _notas);
+    this.notaService.selecionarTodos().subscribe(_notas =>
+      this.notas = _notas
+    );
+    this.categoriaService.selecionarTodos().subscribe(_categoria => 
+      this.categorias = _categoria
+    );
+  }
+
+  filtrarNotasPorCategoria(categoria: Categoria | null){
+    if(categoria == null){
+      this.selecionarTodasNotas();
+      return
+    }
+    this.selecionarNotasPorCategoria(categoria);
+  }
+
+  selecionarNotasPorCategoria(categoria: Categoria) {
+    this.notaService.selecionarTodosComCategoria(categoria).subscribe((notas: Nota[]) => {
+      this.notas = notas
+    })
+  }
+
+  selecionarTodasNotas() {
+    this.notaService.selecionarTodos().subscribe(_notas =>
+      this.notas = _notas
+    );
+  }
+
+  onFiltroSelecionado(categoria: Categoria) {
+    this.filtroCategoria = categoria
+    this.categoriaService.selecionarComCategoria(categoria).subscribe(x => this.categorias == x)
   }
 }
