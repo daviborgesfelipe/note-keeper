514211a0feac0d03ef391ed7a969fa2bc509d955
Davi Borges <davifelipe_300@hotmail.com>
2023-09-17 23:24:42 -0300

    implementa o componente para editar e excluir

---
+++ src/app/components/categorias/excluir-categoria/excluir-categoria.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Categoria } from 'src/app/models/categoria';
+import { Nota } from 'src/app/models/notas';
+import { CategoriaService } from 'src/app/services/categoria.service';
+import { NotaService } from 'src/app/services/nota.service';
+
+@Component({
+  selector: 'app-excluir-categoria',
+  templateUrl: './excluir-categoria.component.html',
+  styleUrls: ['./excluir-categoria.component.css']
+})
+export class ExcluirCategoriaComponent implements OnInit{
+  categoria: Categoria;
+
+  constructor(
+    private notaService: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastrService
+  ){
+  this.categoria = new Categoria('', 0)
+  }
+  ngOnInit(): void {
+    let id: number = parseInt(this.route.snapshot.paramMap.get('id') as string);
+    
+    this.notaService.selecionarPorId(id).subscribe((nota: Categoria) => this.categoria = nota);
+  }
+  
+  excluirCategoria(){
+    this.notaService.excluir(this.categoria).subscribe((categoria: Categoria) => {
+      this.toastService.success(`Categoria ${categoria.titulo} EXCLUIDA com sucesso`, 'Sucesso')
+      this.router.navigate(['/categorias', 'listar'])
+    })
+  }
+}
