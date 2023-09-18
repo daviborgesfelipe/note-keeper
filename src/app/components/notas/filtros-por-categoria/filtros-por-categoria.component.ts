59b7c091c5bd8f9dcd047dcab674ac621d4eb177
Davi Borges <davifelipe_300@hotmail.com>
2023-09-18 15:14:23 -0300

    modifica span e adiciona eventemitter e output

--- src/app/components/notas/filtros-por-categoria/filtros-por-categoria.component.ts
+++ src/app/components/notas/filtros-por-categoria/filtros-por-categoria.component.ts
@@ -1,22 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Categoria } from 'src/app/models/categoria';
+import { CategoriaService } from 'src/app/services/categoria.service';
 
 @Component({
   selector: 'app-filtros-por-categoria',
   templateUrl: './filtros-por-categoria.component.html',
   styleUrls: ['./filtros-por-categoria.component.css']
 })
-export class FiltrosPorCategoriaComponent {
-  @Input({required: true}) categorias: Categoria[] = [];
+export class FiltrosPorCategoriaComponent implements OnInit{
+  @Input({required: true}) categorias: Categoria[];
 
-  @Output() onFiltroSelecionado: EventEmitter<Categoria | null>;
+  @Output() onFiltroSelecionado: EventEmitter<Categoria>;
 
-  constructor(){
+  constructor(private servicoCategoria: CategoriaService){
     this.onFiltroSelecionado = new EventEmitter();
+    this.categorias = []
+  }
+  ngOnInit(): void {
+    this.servicoCategoria.selecionarTodos().subscribe(_categorias => this.categorias == _categorias)
   }
 
   selecionarTodas(){
-    this.onFiltroSelecionado.emit(null);
+    this.onFiltroSelecionado.emit();
   }
 
   selecionarComFiltro(categoria: Categoria){
