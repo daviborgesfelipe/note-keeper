bafc15a6db847d1ea33b9bd8f236fe68b860e6c2
Davi Borges <davifelipe_300@hotmail.com>
2023-09-19 21:24:03 -0300

    adiciona titulo da categoria no cardi

--- src/app/components/notas/card-nota/card-nota.component.ts
+++ src/app/components/notas/card-nota/card-nota.component.ts
@@ -1,17 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Nota } from '../../../models/notas';
+import { Categoria } from 'src/app/models/categoria';
+import { CategoriaService } from 'src/app/services/categoria.service';
 
 @Component({
   selector: 'app-nota',
   templateUrl: './card-nota.component.html',
   styleUrls: ['./card-nota.component.css']
 })
-export class CardNotaComponent {
+export class CardNotaComponent implements OnInit{
   @Input() nota: Nota = {
     id: 0,
     titulo: 'Analise',
     conteudo: 'Analisando',
     tema: 'dark',
-    categoriaId: 2
+    categoriaId: 2,
+    categoria: {
+      titulo: 'Teste',
+      id: 12
+    }
   };
+  
+  @Input() categoria: Categoria = {
+    id: 0,
+    titulo: "Teste"
+  }
+  
+  constructor(private categoriaService: CategoriaService){
+
+  }
+    
+  ngOnInit(): void {
+    this.categoriaService.selecionarPorId(this.nota.categoriaId).subscribe((x: Categoria) => this.categoria = x)
+  }
 }
